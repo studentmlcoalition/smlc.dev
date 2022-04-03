45268152c9205b48cdcd56c6e79d365e87e08167
Saurav Kumar <epicsaurav@gmail.com>
2022-04-02 18:19:38 -0700

    ig

--- src/partials/Instagram.js
+++ src/partials/Instagram.js
@@ -7,7 +7,7 @@ const Instagram = () => {
     return (
         <section className="relative">
             <div className="max-w-6xl mx-auto px-4 sm:px-6">
-                <InstagramFeed token="IGQVJYaDJFQlNYN2RrbVFhVnV5VWNQV25fSUZAXdFh5S0tmOTdnY3pXd1ltZAHl1aDlCcHlJRkZAQbENaU0R4MGF1aVRxREFRcHBxYnd2SEZAnREoyMWJOOGtVOFY4SnpJZAlFKY3liUDFNUlNqa1ppRmRzdQZDZD"  counter="6"/>
+                <InstagramFeed token="IGQVJVaXVkQUVKVHFJcGZAkREE0U1dvenBHY2FZAcy11TmlUZAmJhZA2VXYWdSOHJUUDNWbF9mZAEJlRXhUUnJnZA1NSUnA2RjZAiYWRLSTlZAMHNFQXF6cG00YWg3Mmo4NFBMcmEweFRLZAFZAtWVdmR05XMWpYcAZDZD"  counter="6"/>
             </div>
         </section>
     )
