756216bf51d547a2980b7dd74df726af6ec12ccd
Aadi Chauhan <emailaadichauhan@gmail.com>
2023-08-14 19:51:43 -0700

    updated people page

--- src/partials/MeetTheTeam.js
+++ src/partials/MeetTheTeam.js
@@ -1,98 +1,191 @@
-import React from 'react';
+import React from "react";
 
 function ResourcesBlock() {
-    return (
-        <section>
-            <div className="max-w-6xl mx-auto px-4 sm:px-6">
-                <div className="py-12 md:py-20 border-t border-transparent dark:border-gray-800">
+  return (
+    <section>
+      <div className="max-w-6xl mx-auto px-4 sm:px-6">
+        <div className="py-12 md:py-20 border-t border-transparent dark:border-gray-800">
+          {/* Section header */}
+          <div className="max-w-3xl mx-auto text-center pb-12 md:pb-20">
+            <h2 className="h2 font-red-hat-display mb-4">
+              Meet Our Leadership!
+            </h2>
+          </div>
 
-                    {/* Section header */}
-                    <div className="max-w-3xl mx-auto text-center pb-12 md:pb-20">
-                        <h2 className="h2 font-red-hat-display mb-4">Meet Our Leadership!</h2>
-                    </div>
-
-                    {/* People */}
-                    <div className="max-w-sm mx-auto grid gap-x-8 gap-y-12 sm:grid-cols-3 lg:gap-12 items-start sm:max-w-none md:max-w-2xl lg:max-w-none">
-
-                        <div className="text-center">
-                            <div className="relative inline-flex flex-col mb-4">
-                                <img class="h-48 w-48 object-cover" src={require('../images/photo-michael.jpg').default} alt="Joseph" />
-                            </div>
-                            <blockquote className="text-xl text-gray-600 dark:text-gray-400">
-                                Michael Lutz
-                            </blockquote>
-                            <blockquote className="text-xl text-blue-600 dark:text-gray-400">
-                                Founder
-                            </blockquote>
-                        </div>
-
-                        <div className="text-center">
-                            <div className="relative inline-flex flex-col mb-4">
-                                <img class="h-48 w-48 object-cover" src={require('../images/photo-joseph.jpeg').default} alt="Arjun" />
-                            </div>
-                            <blockquote className="text-xl text-gray-600 dark:text-gray-400">
-                                Joseph Thomas
-                            </blockquote>
-                            <blockquote className="text-xl text-blue-600 dark:text-gray-400">
-                                Co-President
-                            </blockquote>
-                        </div>
-
-                        <div className="text-center">
-                            <div className="relative inline-flex flex-col mb-4">
-                                <img class="h-48 w-48 object-cover" src={require('../images/photo-sanjana.jpg').default} alt="Anousha" />
-                            </div>
-                            <blockquote className="text-xl text-gray-600 dark:text-gray-400">
-                                Sanjana Gadaginmath
-                            </blockquote>
-                            <blockquote className="text-xl text-blue-600 dark:text-gray-400">
-                                Co-President
-                            </blockquote>
-                        </div>
-
-                        <div className="text-center">
-                            <div className="relative inline-flex flex-col mb-4">
-                                <img class="h-48 w-48 object-cover" src={require('../images/photo-aswin.jpeg').default} alt="Anousha" />
-                            </div>
-                            <blockquote className="text-xl text-gray-600 dark:text-gray-400">
-                                Aswin Surya
-                            </blockquote>
-                            <blockquote className="text-xl text-blue-600 dark:text-gray-400">
-                                Resource Director
-                            </blockquote>
-                        </div>
-
-                        <div className="text-center">
-                            <div className="relative inline-flex flex-col mb-4">
-                                <img class="h-48 w-48 object-cover" src={require('../images/photo-rohit.jpg').default} alt="Anousha" />
-                            </div>
-                            <blockquote className="text-xl text-gray-600 dark:text-gray-400">
-                                Rohit Vakkalagadda
-                            </blockquote>
-                            <blockquote className="text-xl text-blue-600 dark:text-gray-400">
-                                Outreach Director
-                            </blockquote>
-                        </div>
-
-                        <div className="text-center">
-                            <div className="relative inline-flex flex-col mb-4">
-                                <img class="h-48 w-48 object-cover" src={require('../images/photo-hayden.jpeg').default} alt="Anousha" />
-                            </div>
-                            <blockquote className="text-xl text-gray-600 dark:text-gray-400">
-                                Hayden Boone
-                            </blockquote>
-                            <blockquote className="text-xl text-blue-600 dark:text-gray-400">
-                                Internal Communications Director
-                            </blockquote>
-                        </div>
-
-                    
-                    </div>
-
-                </div>
+          {/* People */}
+          <div className="max-w-sm mx-auto grid gap-x-8 gap-y-12 sm:grid-cols-3 lg:gap-12 items-start sm:max-w-none md:max-w-2xl lg:max-w-none">
+            <div className="text-center">
+              <div className="relative inline-flex flex-col mb-4">
+                <img
+                  class="h-48 w-48 object-cover"
+                  src={require("../images/photo-aswin.jpeg").default}
+                  alt="Anousha"
+                />
+              </div>
+              <blockquote className="text-xl text-gray-600 dark:text-gray-400">
+                Aswin Surya
+              </blockquote>
+              <blockquote className="text-xl text-blue-600 dark:text-gray-400">
+                President
+              </blockquote>
             </div>
-        </section>
-    );
+
+            <div className="text-center">
+              <div className="relative inline-flex flex-col mb-4">
+                <img
+                  class="h-48 w-48 object-cover"
+                  src={require("../images/photo-aadi.jpg").default}
+                  alt="Aadi"
+                />
+              </div>
+              <blockquote className="text-xl text-gray-600 dark:text-gray-400">
+                Aadi Chauhan
+              </blockquote>
+              <blockquote className="text-xl text-blue-600 dark:text-gray-400">
+                Operations Director
+              </blockquote>
+            </div>
+
+            <div className="text-center">
+              <div className="relative inline-flex flex-col mb-4">
+                <img
+                  class="h-48 w-48 object-cover"
+                  src={require("../images/photo-abhay.jpeg").default}
+                  alt="Aadi"
+                />
+              </div>
+              <blockquote className="text-xl text-gray-600 dark:text-gray-400">
+                Abhay Pokhriyal
+              </blockquote>
+              <blockquote className="text-xl text-blue-600 dark:text-gray-400">
+                Competition Director
+              </blockquote>
+            </div>
+
+            <div className="text-center">
+              <div className="relative inline-flex flex-col mb-4">
+                <img
+                  class="h-48 w-48 object-cover"
+                  src={require("../images/photo-rudransh.jpeg").default}
+                  alt="Aadi"
+                />
+              </div>
+              <blockquote className="text-xl text-gray-600 dark:text-gray-400">
+                Rudransh Singh
+              </blockquote>
+              <blockquote className="text-xl text-blue-600 dark:text-gray-400">
+                Resource Co-Director
+              </blockquote>
+            </div>
+
+            <div className="text-center">
+              <div className="relative inline-flex flex-col mb-4">
+                <img
+                  class="h-48 w-48 object-cover"
+                  src={require("../images/photo-kevin.jpeg").default}
+                  alt="Aadi"
+                />
+              </div>
+              <blockquote className="text-xl text-gray-600 dark:text-gray-400">
+                Kevin Lu
+              </blockquote>
+              <blockquote className="text-xl text-blue-600 dark:text-gray-400">
+                Resource Co-Director
+              </blockquote>
+            </div>
+
+            <div className="text-center">
+              <div className="relative inline-flex flex-col mb-4">
+                <img
+                  class="h-48 w-48 object-cover"
+                  src={require("../images/photo-aarush.jpeg").default}
+                  alt="Aadi"
+                />
+              </div>
+              <blockquote className="text-xl text-gray-600 dark:text-gray-400">
+                Aarush Gupta
+              </blockquote>
+              <blockquote className="text-xl text-blue-600 dark:text-gray-400">
+                Resource Co-Director
+              </blockquote>
+            </div>
+
+            <div className="text-center">
+              <div className="relative inline-flex flex-col mb-4">
+                <img
+                  class="h-48 w-48 object-cover"
+                  src={require("../images/photo-matthew.jpeg").default}
+                  alt="Aadi"
+                />
+              </div>
+              <blockquote className="text-xl text-gray-600 dark:text-gray-400">
+                Matthew Bannon
+              </blockquote>
+              <blockquote className="text-xl text-blue-600 dark:text-gray-400">
+                Media Director
+              </blockquote>
+            </div>
+          </div>
+
+          {/* Alumni */}
+          <div className="max-w-3xl mx-auto text-center pb-12 pt-10 md:pb-20">
+            <h3 className="h3 font-red-hat-display mb-4 text-gray-400">
+              Alumni Leadership
+            </h3>
+          </div>
+          <div className="max-w-sm mx-auto grid gap-x-8 gap-y-12 sm:grid-cols-3 lg:gap-12 items-start sm:max-w-none md:max-w-2xl lg:max-w-none">
+            <div className="text-center">
+              <div className="relative inline-flex flex-col mb-4">
+                <img
+                  class="h-48 w-48 object-cover"
+                  src={require("../images/photo-michael.jpg").default}
+                  alt="Joseph"
+                />
+              </div>
+              <blockquote className="text-xl text-gray-600 dark:text-gray-400">
+                Michael Lutz
+              </blockquote>
+              <blockquote className="text-xl text-blue-600 dark:text-gray-400">
+                Founder
+              </blockquote>
+            </div>
+
+            <div className="text-center">
+              <div className="relative inline-flex flex-col mb-4">
+                <img
+                  class="h-48 w-48 object-cover"
+                  src={require("../images/photo-joseph.jpeg").default}
+                  alt="Arjun"
+                />
+              </div>
+              <blockquote className="text-xl text-gray-600 dark:text-gray-400">
+                Joseph Thomas
+              </blockquote>
+              <blockquote className="text-xl text-blue-600 dark:text-gray-400">
+                Co-President
+              </blockquote>
+            </div>
+
+            <div className="text-center">
+              <div className="relative inline-flex flex-col mb-4">
+                <img
+                  class="h-48 w-48 object-cover"
+                  src={require("../images/photo-sanjana.jpg").default}
+                  alt="Anousha"
+                />
+              </div>
+              <blockquote className="text-xl text-gray-600 dark:text-gray-400">
+                Sanjana Gadaginmath
+              </blockquote>
+              <blockquote className="text-xl text-blue-600 dark:text-gray-400">
+                Co-President
+              </blockquote>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 }
 
 export default ResourcesBlock;
